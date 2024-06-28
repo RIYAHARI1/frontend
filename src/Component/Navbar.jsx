410a8a38982667a4e510993013b4203705f4eedd
RIYAHARI1 <riyahari200@gmail.com>
2024-06-28 15:27:09 +0530

    Add files via upload

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,15 @@
+function Nav(){
+  return(
+      <div className="navbar">
+      
+          <h1 style={{color:"black"}}>F&A COMPANY</h1>
+         
+      
+    </div>
+
+      
+     
+      
+  )
+}
+export default Nav;
---
+++ src/Component/portfolio.jsx
@@ -0,0 +1,26 @@
+function Port()
+{
+    return(
+        <>
+    
+    
+     
+
+      <div className="portfolio">
+        
+      <div>
+            <h1>MATRIMONIAL</h1>
+          <h2>JOIN US TO FIND YOUR MATE</h2>
+        </div>
+          
+
+        
+        
+      </div>
+
+    
+        
+         </>
+    )
+}
+export default Port;
